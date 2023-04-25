609cc56e72939e5648d52989adfed44887bf05dd
IAKabanov <ivan.a.kabanov@gmail.com>
2023-04-25 20:39:34 +0200

    Pros     - Logic works properly     - Architecture built according Clean Architecture Cons     - Not intuitive UI Comments     Need to fix a few UI bugs. Maybe TreeView should remember its state and expand the only nodes, that were expanded before. Now it's always expanded.

--- app/src/main/java/com/ikabanov/cache/data/cache/ElementCache.kt
+++ app/src/main/java/com/ikabanov/cache/data/cache/ElementCache.kt
@@ -4,10 +4,9 @@ package com.ikabanov.cache.data.cache
  * ElementCache is a model of cache element. Will be united to Element soon.
  */
 data class ElementCache(var name: String) : Comparable<ElementCache> {
-    var deleted: Boolean = false
     var parent: ElementCache? = null
     var level: Int = 0
-        private set
+    var deleted: Boolean = false
     var children: MutableList<ElementCache> = mutableListOf()
 
     constructor(name: String, parentName: String) : this(name) {
